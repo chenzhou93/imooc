464a8782d98f5a2ffb411bf4bd321ee7c8021adb
Chen Zhou <chenzhoux93@gmail.com>
2020-06-07 13:11:12 -0400

    Section 2.7-2.9

--- Data Structure/Ch02-Arrays/main1.cpp
+++ Data Structure/Ch02-Arrays/main1.cpp
@@ -12,7 +12,19 @@ int main(){
     for(int i=0; i<10; i++){
         array2.addLast(i);
     }
+    for(int i=0; i<10; i++){
+        cout << array2[i] << " ";
+    }
+    cout << endl;
     array2.add(1, 100);
+    for(int i=0; i<10; i++){
+        cout << array2[i] << " ";
+    }
+    cout << endl;
     array2.addFirst(101);
+    for(int i=0; i<10; i++){
+        cout << array2[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
