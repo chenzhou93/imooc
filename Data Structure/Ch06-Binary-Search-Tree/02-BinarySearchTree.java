7abd8a226611197abe518e2788cc0aae6a29a192
Chen Zhou <chenzhoux93@gmail.com>
2020-06-22 08:13:24 -0400

    Ch06 1-2 BST

---
+++ Data Structure/Ch06-Binary-Search-Tree/02-BinarySearchTree.java
@@ -0,0 +1,31 @@
+public class BinarySearchTree<E extends Comparable<E>> {
+    private class Node{
+        public E e;
+        public Node left;
+        public Node right;
+
+        public Node(E e){
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public BinarySearchTree(){
+        root = null;
+        size = 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    
+}
