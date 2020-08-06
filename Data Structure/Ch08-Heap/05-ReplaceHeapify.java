4e56753959d08ec4a07d5729e41bde6ca3f75d5c
Chen Zhou <chenzhoux93@gmail.com>
2020-08-05 21:13:08 -0400

    Ch08 priority queue java code

---
+++ Data Structure/Ch08-Heap/05-ReplaceHeapify.java
@@ -0,0 +1,97 @@
+public class MaxHeap<E extends Comparable<E>>{
+    private Array<E> data;
+
+    public MaxHeap(int capacity){
+        data = new Array<>(capacity);
+    }
+
+    public MaxHeap(){
+        data = new Array<>();
+    }
+
+    public MaxHeap(E[] arr){
+        data = new Array<>(arr);
+        for(int i=parent(arr.length-1); i >=0; i--){
+            siftDown(i);
+        }
+    }
+
+    public int size(){
+        return data.getSize();
+    }
+
+    public boolean isEmpty(){
+        return data.isEmpty();
+    }
+
+    private int parent(int index){
+        if(index == 0){
+            throw new IllegalArgumentException("index-0");
+        }
+        return (index - 1)/2;
+    }
+
+    private int leftChild(int index){
+        return index * 2 + 1;
+    }
+
+    private int rightChild(int index){
+        return index * 2 + 2;
+    }
+
+    public void add(E e){
+        data.addLast(E e);
+        siftUp(data.getSize() - 1);
+    }
+    
+    private void siftUp(int k){
+        while(k>0 && data.get(parent(k)).compareTo(data.get(k)) < 0){
+            data.swap(k, parent(k));
+            k = parent(k);
+        }
+    }
+
+    public E findMax(){
+        if(data.getSize()==0){
+            throw;
+        }
+        return data.get(0);
+    }
+
+    public E extractMax(){
+        E ret = findMax();
+        data.swap(0, data.getSize()-1);
+        data.removeLast();
+        siftDown(0);
+        return ret;
+    }
+
+    private siftDown(int k){
+        while(leftChild(k) < data.getSize()){
+            int j = leftChild(k);
+            if(j + 1 < data.getSize() 
+            && data.get(j+1).compareTo(data.get(j)>0)){
+                j = rightChild(k);
+            }
+            //data[j] is max of left child and right child
+
+            if(data.get(k).compareTo(data.get(j)) >= 0){// k is larger than left and right
+                break;
+            }
+
+            data.swap(k, j);
+            k = j;//next round
+        }
+    }
+
+    public E replace(E e){
+        E ret = findMax();
+        data.set(0, e);
+        siftDown(0);
+        return ret;
+    }
+
+
+
+
+}
